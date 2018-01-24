bc734dae6de75a12d8f7662ef098d4d5c764b061
Maylin Horchler <mahorchler@ctemc.org>
2018-01-23 23:34:54 -0500

    Added comments

--- python/Fake_Database_Creator.py
+++ python/Fake_Database_Creator.py
@@ -2,14 +2,25 @@ import sqlite3
 import uuid
 
 def create_connection(database):
+    """ Connects to database. Returns connection. Uses current directory. """    
     connection = sqlite3.connect(database)
     return connection
 
 def create_user_database():
+    """ 
+        Creates table of users with hardcoded information in data.db database 
+        Uses Current Working Directory
+            
+    """
+    #Get connection
     connection = create_connection('data.db')
     cursor = connection.cursor()
+    
+    #Create Table
     cursor.execute('CREATE TABLE users (ID TEXT PRIMARY KEY, Username TEXT, First TEXT, Last TEXT, Password TEXT, Email TEXT, Bio TEXT, Image TEXT, Selling TEXT, Invested TEXT, Looking TEXT, Balance INTEGER)')
+    #Generate User IDs
     userIds = [str(uuid.uuid4()), str(uuid.uuid4()), str(uuid.uuid4()), str(uuid.uuid4()), str(uuid.uuid4()), str(uuid.uuid4()), str(uuid.uuid4()), str(uuid.uuid4()), str(uuid.uuid4()), str(uuid.uuid4())]
+    #Hardcode User Information
     user1 = [userIds[0], 'Maylin', 'Maylin', 'Horchler', '123456', 'mahorchler@ctemc.org', 'I am a pianist from Morganville, New Jersey.', userIds[0] + '.img','','','',1000000]
     user2 = [userIds[1], 'Bryan', 'Bryan', 'Yao', '234567', 'bryao@ctemc.org', 'I am a software engineer from Marlboro, New Jersey.', userIds[1] + '.img','','','',10000000]
     user3 = [userIds[2], 'Zach', 'Zachary', 'Glasser', '345678', 'zaglasser@ctemc.org', 'I am CEO of Price Waterhouse, currently living in New York City.', userIds[2] + '.img','','','',100000000]
@@ -20,6 +31,7 @@ def create_user_database():
     user8 = [userIds[7], 'Anna', 'Anna', 'Cai', '891234', 'ancai@ctemc.org', 'I am a historian who travels the world, currently living in Xi\'An, China.', userIds[7] + '.img','','','',4000000]
     user9 = [userIds[8], 'Steven', 'Steven', 'Liu', '912345', 'stliu@ctemc.org', 'I am currently developing the world\'s most powerful nuclear weapons. My location is secret.', userIds[8] + '.img','','','',250000]
     user0 = [userIds[9], 'Zach', 'Zachary', 'Colucci', '13579', 'zacolucci@ctemc.org', 'I\'m a comedian living in Las Vegas.', userIds[9] + '.img','','','',5000000]
+    #Create Users
     add_user(user1)
     add_user(user2)
     add_user(user3)
@@ -31,16 +43,29 @@ def create_user_database():
     add_user(user9)
     add_user(user0)
     
+    #Save and close connection
     connection.commit()
     connection.close()
 
 def create_house_database():
+    """ 
+        Creates table of houses with hardcoded information in data.db database 
+        Uses Current Working Directory
+            
+    """
+    
+    #Get Connection
     connection = create_connection('data.db')
     cursor = connection.cursor()
+    
+    #Create Table of HousesForSale
     cursor.execute('CREATE TABLE HousesForSale (House TEXT PRIMARY KEY, Seller TEXT, Address TEXT, Price INTEGER, Size INTEGER, Bedrooms INTEGER, Bath REAL, Description TEXT, Investors TEXT, Image TEXT, Images TEXT)')
+    
+    #Get User ID's
     cursor.execute('SELECT ID FROM users')
     peopleIds = cursor.fetchall()
 
+    #Variables will hold user ID's that will be the sellers of the houses that are hardcoded as fake data
     Maylin = ''
     Bryan = ''
     Zach = ''
@@ -49,10 +74,13 @@ def create_house_database():
     Sathya = ''
 
     for idee in peopleIds:
+        #Get username
         cursor.execute('SELECT Username FROM users WHERE ID = ?',(idee[0],))
         username = cursor.fetchall()
+        #Get password
         cursor.execute('SELECT Password FROM users WHERE ID = ?',(idee[0],))
         password = cursor.fetchall()
+        #If the person being searched for is found, save their UUID into their corresponding variable
         if str(username[0])[3:-3] == 'Maylin':
             Maylin = idee[0]
         if str(username[0])[3:-3] == 'Bryan':
@@ -65,8 +93,10 @@ def create_house_database():
             Karena = idee[0]
         if str(username[0])[3:-3] == 'Sathya':
             Sathya = idee[0]
-        
+    
+    #Generate UUID's for the houses    
     saleHousesIds = [str(uuid.uuid4()),str(uuid.uuid4()),str(uuid.uuid4()),str(uuid.uuid4()),str(uuid.uuid4()),str(uuid.uuid4()),str(uuid.uuid4()),str(uuid.uuid4()),str(uuid.uuid4()),str(uuid.uuid4())]    
+    #Hardcode house information
     house1 = [saleHousesIds[0], Maylin, '240 Deerfield Rd, Morganville, NJ, 07751', 1000000, 4200, 5, 4.5, 'This is my house.','','','']
     house2 = [saleHousesIds[1], Zach, '1912 Wilcox Avenue, Monterey Park, CA, 91755', 600000, 3100, 3, 2.5, 'This is my first house in California.','','','']
     house3 = [saleHousesIds[2], Zach, '1909 Oakgate St, Monterey Park, CA, 91755', 650000, 3100, 3, 3, 'This is my second house in California.','','','']
@@ -77,6 +107,7 @@ def create_house_database():
     house8 = [saleHousesIds[7], Maylin, '1 Menzel Lane, Holmdel, NJ, 07733', 600000, 2800, 3, 2.5, 'This is my old house.','','','']
     house9 = [saleHousesIds[8], Karena, '85-44 Homelawn St, Jamaica, NY, 11432', 1250000, 3000, 3, 2.5, 'This is a house in New York.','','','']
     house0 = [saleHousesIds[9], Sathya, '407 Centerville St, Middleburg, PA, 17842', 500000, 2900, 3, 3, 'This is a house in Pennsylvania.','','','']
+    #Create houses
     add_house(house1)
     add_house(house2)
     add_house(house3)
@@ -88,23 +119,42 @@ def create_house_database():
     add_house(house9)
     add_house(house0)
     
+    #Save and close connection
     connection.commit()
     connection.close()
 
 def add_user(user):
+    """
+        Receives list of information regarding user.
+        Inserts given information into database.
+        Uses current working directory.
+    """
+    #Get Connection
     connection = create_connection('data.db')
     cursor = connection.cursor()
+    
+    #Add information to database
     cursor.execute("INSERT INTO users (ID, Username, First, Last, Password, Email, Bio, Image, Selling, Invested, Looking, Balance) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",(user[0],user[1],user[2],user[3],user[4],\
                     user[5],user[6],user[7],user[8],user[9],user[10],user[11]))
     
+    #Save and close connection
     connection.commit()
     connection.close()
 
 def add_house(house):
+    """
+        Receives list of information regarding house.
+        Inserts given information into database.
+        Uses current working directory.
+    """
+    #Get Connection
     connection = create_connection('data.db')
     cursor = connection.cursor()
+    
+    #Add information to database
     cursor.execute("INSERT INTO HousesForSale (House, Seller, Address, Price, Size, Bedrooms, Bath, Description, Investors, Image, Images) VALUES (?,?,?,?,?,?,?,?,?,?,?)", (house[0], house[1], house[2],\
                     house[3], house[4], house[5], house[6], house[7], house[8], house[9], house[10]))
-
+    
+    #Save and close connection
     connection.commit()
     connection.close()
--- python/querying.py
+++ python/querying.py
@@ -2,58 +2,107 @@ import sqlite3
 import uuid
 
 def create_connection(database):
+    """ Connects to database. Returns connection. Uses current directory. """
     connection = sqlite3.connect(database)
     return connection
 
 def login(username, password):
+    """
+        Receives username and password.
+        Checks to see if username and password are associated with the same UUID.
+        If so, returns all information of the user.
+        If not, returns false.
+        Uses current directory.
+    """
+    #Get Connection
     connection = create_connection('data.db')
     cursor = connection.cursor()
+    
+    #Fetch all passwords associated with given username
     cursor.execute('SELECT Password FROM users WHERE Username = ?',(username,))
     passwords = cursor.fetchall()
     
     for word in passwords:
+        #If one of the passwords corresponds with the given password, return the information of the user
         if word[0] == password:
             cursor.execute('SELECT * FROM users WHERE Username = ? AND Password = ?',(username,password))
             user_information = cursor.fetchall()
+            connection.close()
             return user_information
         
+    #Close connection
     connection.close()
     
+    #Return False if username and password don't match up
     return False
 
 def get_all_houses():
+    """
+        Returns ID's of all houses.
+        Uses current directory.
+    """
+    #Get Connection
     connection = create_connection('data.db')
     cursor = connection.cursor()
+    
+    #Fetch ID's of all houses
     cursor.execute('SELECT House FROM HousesForSale')
     information = cursor.fetchall()
     
+    #Close connection
     connection.close()
     
     return information
 
 def get_house_information(house_id):
+    """
+       Receives the ID of an already-existing house
+       Returns all of the information about that house.
+       Uses current directory.
+    """
+    #Get Connection
     connection = create_connection('data.db')
     cursor = connection.cursor()
+    
+    #Fetch information about house with given ID
     cursor.execute('SELECT * FROM HousesForSale WHERE House = ?',(house_id,))
     information = cursor.fetchall()
     
+    #Close Connection
     connection.close()
     
     return information
 
 def get_user_information(id_number):
+    """
+       Receives the ID of an already-existing user
+       Returns all of the information about that user.
+       Uses current directory.
+    """
+    #Get Connection
     connection = create_connection('data.db')
     cursor = connection.cursor()
+    
+    #Fetch information about user with given ID
     cursor.execute('SELECT * FROM users WHERE ID = ?',(id_number,))
     information = cursor.fetchall()
     
+    #Close Connection
     connection.close()
     
     return information
 
 def edit_user_information(user_id, parameter, new_value):
+    """
+       Receives the ID of an already-existing user, the parameter to be changed, and the value to change it to.
+       Changes the parameter of that user to the new value.
+       Uses current directory.
+    """
+    #Get Connection
     connection = create_connection('data.db')
     cursor = connection.cursor()
+    
+    #Determine which parameter is to be changed, and change it
     if parameter == 'Username':
         cursor.execute('UPDATE users SET Username=? WHERE ID = ?',(new_value,user_id))
         
@@ -66,12 +115,21 @@ def edit_user_information(user_id, parameter, new_value):
     if parameter == 'Bio':
         cursor.execute('UPDATE users SET Bio=? WHERE ID = ?',(new_value,user_id))
     
+    #Save and close connection
     connection.commit()
     connection.close()
     
 def edit_house_information(house_id, parameter, new_value):
+    """
+       Receives the ID of an already-existing house, the parameter to be changed, and the value to change it to.
+       Changes the parameter of that house to the new value.
+       Uses current directory.
+    """
+    #Get Connection
     connection = create_connection('data.db')
     cursor = connection.cursor()
+    
+    #Determine which parameter is to be changed, and change it
     if parameter == 'Seller':
         cursor.execute('UPDATE HousesForSale SET Seller=? WHERE House = ?',(new_value,house_id))
     if parameter == 'Address':
@@ -87,41 +145,84 @@ def edit_house_information(house_id, parameter, new_value):
     if parameter == 'Description':
         cursor.execute('UPDATE HousesForSale SET Description=? WHERE House = ?',(new_value,house_id))
     
+    #Save and close connection
     connection.commit()
     connection.close()
 
 def add_new_user(user):
+    """
+        Receives list of information regarding a new user.
+        Generates new UUID for them.
+        Inserts UUID and given information into database.
+        Uses current working directory.
+    """
+    #Generate new UUID
     ID = str(uuid.uuid4())
+    
+    #Get Connection
     connection = create_connection('data.db')
     cursor = connection.cursor()
+    
+    #Insert information into data table
     cursor.execute("INSERT INTO users (ID, Username, First, Last, Password, Email, Bio, Image, Selling, Invested, Looking, Balance) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",(ID,user[0],user[1],user[2],user[3],\
                     user[4],user[5],ID + '.img',user[6],user[7],user[8],user[9]))
     
+    #Save and close connection
     connection.commit()
     connection.close()
 
 def add_new_house(house):
+    """
+        Receives list of information regarding a new house.
+        Generates new UUID for it.
+        Inserts UUID and given information into database.
+        Uses current working directory.
+    """
+    #Generate new UUID
     ID = str(uuid.uuid4())
+    
+    #Get Connection
     connection = create_connection('data.db')
     cursor = connection.cursor()
+    
+    #Insert information into data table
     cursor.execute("INSERT INTO HousesForSale (House, Seller, Address, Price, Size, Bedrooms, Bath, Description, Investors, Image, Images) VALUES (?,?,?,?,?,?,?,?,?,?,?)",(ID,house[0],house[1],house[2],house[3],\
                     house[4],house[5],house[6],'',ID + '.img',''))
     
+    #Save and close connection
     connection.commit()
     connection.close()
 
 def delete_user(ID):
+    """
+        Receives a user ID of an already-existing user.
+        Deletes user's information from data table.
+        Uses current working directory.
+    """
+    #Get Connection
     connection = create_connection('data.db')
     cursor = connection.cursor()
+    
+    #Delete Information
     cursor.execute("DELETE FROM users WHERE ID=?",(ID,))
     
+    #Save and close connection
     connection.commit()
     connection.close()
 
 def delete_house(ID):
+    """
+        Receives a house ID of an already-existing house.
+        Deletes house's information from data table.
+        Uses current working directory.
+    """
+    #Get Connection
     connection = create_connection('data.db')
     cursor = connection.cursor()
+    
+    #Delete Information
     cursor.execute("DELETE FROM HousesForSale WHERE House=?",(ID,))
     
+    #Save and close connection
     connection.commit()
     connection.close()
