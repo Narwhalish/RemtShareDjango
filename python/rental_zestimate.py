b22bacf0a468b97842b8a9d38e6909309df38c70
Zach Glasser <zaglasser@ctemc.org>
2018-06-07 02:01:30 -0400

    changed xml to dict

--- python/rental_zestimate.py
+++ python/rental_zestimate.py
@@ -1,11 +1,14 @@
 import requests
+import xmltodict
+import json
 
 def rental_zestimate(url, zipcode):
     one = url.index(zipcode +"/")+6
     two = url.index("_zpid")
     url = url[one:two]
     response = requests.get("http://www.zillow.com/webservice/GetZestimate.htm?zws-id=X1-ZWz1gfdi9irh1n_1sm5d&zpid="+url+"&rentzestimate=true")
-    return(response.content)
+    xpars = xmltodict.parse(response.text)
+    return(xpars)
     
 def parse_zestimate(response):
-    
+    print response
